d422b7f2e95d0b0805615ddbfd60ea0069762fde
k-kumar-01 <kushalkumargupta4@gmail.com>
2021-01-14 18:39:07 +0530

    chore and feat
    chore:added react-bootstrap
    feat:added skeleton basic components and css
    Signed-off-by: k-kumar-01 <kushalkumargupta4@gmail.com>

---
+++ components/Skeleton/SkeletonElement.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import styles from "./Skeleton.module.css";
+
+interface Props {
+  type: string;
+}
+
+const SkeletonElement: React.FC<Props> = ({ type }) => {
+  const classes = `${styles.skeleton} ${styles[type]}`;
+
+  return <div className={classes}></div>;
+};
+
+export default SkeletonElement;
